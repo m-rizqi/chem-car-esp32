a6f8ed4041af33bc39ef2098da8c83412630e824
Muhammad Rizqi <mrizqi070502@gmail.com>
2023-06-11 16:57:22 +0700

    feat: redesign webserver

--- main/main.ino
+++ main/main.ino
@@ -26,7 +26,6 @@ size_t float_size = sizeof(float);
 cppQueue potentiometer_queue(int_size, ARRAY_MAX_VALUES, QUEUE_IMPLEMENTATION);
 
 #define DHT_PIN 4
-// #define DHT_TYPE DHT11
 #define DHT_TYPE DHT22
 DHT dht(DHT_PIN, DHT_TYPE);
 
@@ -55,57 +54,181 @@ size_t time_size = sizeof(Time);
 cppQueue time_queue(time_size, ARRAY_MAX_VALUES, QUEUE_IMPLEMENTATION);
 
 const char index_html[] PROGMEM = R"rawliteral(
-  <!DOCTYPE HTML><html>
-<head>
-  <title>ESP Web Server</title>
-</head>
-<body>
-  <h1>WebSocket</h1>
-  <h2>Time</h2>
-  <h3 id="time"></h3>
-  <h2>Potentiometer</h2>
-  <h3 id="potentio"></h3>
-  <h2>Humidity</h2>
-  <h3 id="humidity"></h3>
-  <h2>Temperature</h2>
-  <h3 id="temperature"></h3>
-<script>
-  var gateway = `ws://${window.location.hostname}/ws`;
-  var websocket;
-  window.addEventListener('load', onLoad);
-  function initWebSocket() {
-    console.log('Trying to open a WebSocket connection...');
-    websocket = new WebSocket(gateway);
-    websocket.onopen    = onOpen;
-    websocket.onclose   = onClose;
-    websocket.onmessage = onMessage; 
-  }
-  function onOpen(event) {
-    console.log('Connection opened');
-  }
-  function onClose(event) {
-    console.log('Connection closed');
-    setTimeout(initWebSocket, 2000);
-  }
-  function onMessage(event) {
-    console.log(event.data);
-    //document.getElementById('data').innerHTML = event.data;
-    const obj = JSON.parse(event.data);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sensor Monitor</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+      crossorigin="anonymous"
+    />
+    <style>
+      body {
+        background-color: #f5f7f9;
+      }
 
-    const timeString = JSON.stringify(obj.times);
-    document.getElementById('time').innerHTML = timeString;
-    const potentiometerString = JSON.stringify(obj.potentiometer);
-    document.getElementById('potentio').innerHTML = potentiometerString;
-    const humidityString = JSON.stringify(obj.humidity);
-    document.getElementById('humidity').innerHTML = humidityString;   
-    const temperatureString = JSON.stringify(obj.temperature);
-    document.getElementById('temperature').innerHTML = temperatureString;     
-  }
-  function onLoad(event) {
-    initWebSocket();
-  }
-</script>
-</body>
+      .header {
+        overflow: hidden;
+        background-color: #fff;
+        padding: 15px 10px;
+        text-align: center;
+      }
+
+      .header h4 {
+        color: black;
+        font-weight: 600;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="header">
+      <h4 href="" class="heading">Sensor Monitor</h4>
+    </div>
+
+    <div class="p-3">
+      <div class="bg-white rounded-2 p-3">
+        <div class="row">
+          <div class="col-md">
+            <div class="fw-medium fs-6" style="color: #727272">
+              Potentiometer
+            </div>
+            <div class="fw-bold fs-1 mt-2">75°</div>
+            <div style="margin-top: -8px">Degree</div>
+          </div>
+          <div class="col-md">
+            <canvas id="potentiometer_chart"></canvas>
+          </div>
+        </div>
+      </div>
+
+      <div class="bg-white rounded-2 p-3 mt-3">
+        <div class="row">
+          <div class="col-md">
+            <div class="fw-medium fs-6" style="color: #727272">Temperature</div>
+            <div class="fw-bold fs-1 mt-2">24°</div>
+            <div style="margin-top: -8px">Celcius</div>
+          </div>
+          <div class="col-md">
+            <canvas id="temperature_chart"></canvas>
+          </div>
+        </div>
+      </div>
+
+      <div class="bg-white rounded-2 p-3 mt-3">
+        <div class="row">
+          <div class="col-md">
+            <div class="fw-medium fs-6" style="color: #727272">Humidity</div>
+            <div class="fw-bold fs-1 mt-2">92%</div>
+          </div>
+          <div class="col-md">
+            <canvas id="humidity_chart"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script>
+      const potentiometer_ctx = document.getElementById("potentiometer_chart");
+      var potentiometer_chart = new Chart(potentiometer_ctx, {
+        type: "line",
+        data: {
+          labels: [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+          ],
+          datasets: [
+            {
+              data: [86, 114, 106, 106, 107, 111, 133],
+              label: "Degree of Angle",
+              borderColor: "rgb(62,149,205)",
+              backgroundColor: "rgb(62,149,205,0.1)",
+            },
+          ],
+        },
+      });
+
+      const temperature_ctx = document.getElementById("temperature_chart");
+      var temperature_chart = new Chart(temperature_ctx, {
+        type: "line",
+        data: {
+          labels: [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+          ],
+          datasets: [
+            {
+              data: [70, 90, 44, 60, 83, 90, 100],
+              label: "Temperature",
+              borderColor: "rgb(60,186,159)",
+              backgroundColor: "rgb(60,186,159,0.1)",
+            },
+          ],
+        },
+      });
+
+      const humidity_ctx = document.getElementById("humidity_chart");
+      var humidity_chart = new Chart(humidity_ctx, {
+        type: "line",
+        data: {
+          labels: [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+          ],
+          datasets: [
+            {
+              data: [10, 21, 60, 44, 17, 21, 17],
+              label: "Humidity",
+              borderColor: "rgb(255,165,0)",
+              backgroundColor: "rgb(255,165,0,0.1)",
+            },
+          ],
+        },
+      });
+
+      var gateway = `ws://${window.location.hostname}/ws`;
+      var websocket;
+      window.addEventListener("load", onLoad);
+      function initWebSocket() {
+        console.log("Trying to open a WebSocket connection...");
+        websocket = new WebSocket(gateway);
+        websocket.onopen = onOpen;
+        websocket.onclose = onClose;
+        websocket.onmessage = onMessage;
+      }
+      function onOpen(event) {
+        console.log("Connection opened");
+      }
+      function onClose(event) {
+        console.log("Connection closed");
+        setTimeout(initWebSocket, 2000);
+      }
+      function onMessage(event) {
+        console.log(event.data);
+      }
+      function onLoad(event) {
+        initWebSocket();
+      }
+    </script>
+  </body>
 </html>
 )rawliteral";
 
